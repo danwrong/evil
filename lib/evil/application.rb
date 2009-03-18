12b6873828948351cc146c8bfca15164afbc4150
Dan Webb <dan@danwebb.net>
2009-03-18 21:55:47 +0000

    .gitignore in

--- lib/evil/application.rb
+++ lib/evil/application.rb
@@ -2,16 +2,20 @@ require 'sinatra'
 require 'evil/models'
 
 configure do
+  Sinatra.register Evil::Models
+  
   set :sessions => true,
       :static   => File.join(Evil.app_root, 'public'),
       :views    => File.join(Evil.gem_root, 'views'),
       :haml     => { :format => :html5 }
       
-  Sinatra.register Evil::Models
   connect_to_database
 end
 
+include Evil::Models
+
 get '/admin' do
+  @thing = Whitelist.find(:first).pattern
   haml :index
 end
       
