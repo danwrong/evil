c79c7ed6b2b240a44f0aa23ffaff60e70d071f0b
Dan Webb <dan@danwebb.net>
2009-04-07 23:01:28 +0100

    template model validates liquid syntax

--- lib/evil/models/template.rb
+++ lib/evil/models/template.rb
@@ -1,3 +1,4 @@
+require 'liquid'
 module Evil
   
   module Models
@@ -8,10 +9,19 @@ module Evil
       validates_presence_of   :route, :source
       validates_uniqueness_of :route
       validates_format_of     :route, :with => /^((\/)|(((\/|\.):?[a-z0-9\_]+)+))$/
+      validate :liquid_syntax
       
       has_many :cached_pages
       
       named_scope :in_order, :order => 'position ASC'
+      
+      private
+      
+      def liquid_syntax
+        Liquid::Template.parse(source)
+      rescue Liquid::SyntaxError => error
+        errors.add(:source, "has syntax error: #{error.message}")
+      end
     end
     
   end
